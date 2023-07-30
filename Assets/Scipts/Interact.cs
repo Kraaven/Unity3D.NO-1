2ce1ab6ac0c507c2dda72a7e639368c60ee8b64d
Kraaven <dhruv.bhojaraj@gmail.com>
2023-07-30 19:10:30 +0530

    added item
    Added a Log, also allowed dropping configuration in the object info script. Trees now drop Logs. Breaking is functional, also configurable

--- Assets/Scipts/Interact.cs
+++ Assets/Scipts/Interact.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class Interact : MonoBehaviour
 {
@@ -11,6 +13,7 @@ public class Interact : MonoBehaviour
     public bool isbreaking = false;
 
     public float breaktime = 0;
+    public GameObject prev;
 
     public float breaktick = 0;
     // Start is called before the first frame update
@@ -18,22 +21,81 @@ public class Interact : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        prev = touch;
         RaycastHit Hit;
         if (Physics.Raycast(camera.transform.position, camera.transform.TransformDirection(Vector3.forward), out Hit, 3.5f))
-        {
+        {    
             touch = Hit.transform.gameObject;
             Debug.Log(touch.GetComponent<ObjectInfo>().Name);
             
         }
-        
-        
-        if (Input.GetMouseButton(0))
+        else
         {
-            Debug.Log("Button Down");
+            touch = null;
+        }
+
+
+        if (Input.GetMouseButton(0) && touch != null)
+        {
+
+            if (prev != touch)
+            {
+                reset();
+                isbreaking = false;
+                breaktick = 0;
+                breaktime = 0;
+            }
+
+            if (touch.GetComponent<ObjectInfo>().breakable)
+            {
+                isbreaking = true;
+            }
+
+            breaktime = touch.GetComponent<ObjectInfo>().BreakTime;
             
+            // while (isbreaking)
+            // {
+            //     if (!touch.GetComponent<ObjectInfo>().breakable)
+            //     {
+            //         break;
+            //     }
+            //     breaktick += Time.deltaTime;
+            //     // if (breaktick >= breaktime)
+            //     // {
+            //     //     Destroy(touch);
+            //     //     isbreaking = false;
+            //     //     touch = null;
+            //     // }
+            // }
+            breaktick += Time.deltaTime;
+            if (breaktick > breaktime && isbreaking)
+            {
+                touch.GetComponent<ObjectInfo>().breakobject(gameObject);
+                reset();
+            }
+
 
 
         }
-        
+        if (Input.GetMouseButtonUp(0))
+        {
+            reset();
+        }
+
+        // if (Input.GetMouseButtonUp(0))
+        // {
+        //     isbreaking = false;
+        //     breaktick = 0;
+        //     breaktime = 0;
+        // }
+
+    }
+
+    private void reset()
+    {
+        isbreaking = false;
+        breaktick = 0;
+        breaktime = 0;
+        touch = null;
     }
 }
--- Assets/Scipts/ObjectInfo.cs
+++ Assets/Scipts/ObjectInfo.cs
@@ -6,5 +6,27 @@ using UnityEngine;
 public class ObjectInfo : MonoBehaviour
 {
     public string Name;
+    public float BreakTime;
+    public bool breakable;
 
+    public bool drops;
+    public GameObject dropped1 = null;
+    public int numdrop1;
+    public GameObject dropped2 = null;
+    public int numdrop2;
+
+    public void breakobject(GameObject origin)
+    {
+        Vector3 spawn = origin.transform.position; 
+        if (drops)
+        {
+            for (int i = 0; i < numdrop1; i++)
+            {
+                Debug.Log(spawn.z + 0.25f * i);
+                Instantiate(dropped1, new Vector3(spawn.x, spawn.y + i, spawn.z), Quaternion.identity);
+            }
+        }
+        
+        Destroy(gameObject);
+    }
 }
